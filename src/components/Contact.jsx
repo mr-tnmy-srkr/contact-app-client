4836309fa2bf6646fa6a64e577cdbb7cc2183ba6
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-02-16 19:38:08 +0530

    post and edit contact done by fake server

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,19 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
 
 export default function Contact() {
-  const contact = {
+  /*  const contact = {
     first: "Your",
     last: "Name",
     avatar: "https://placekitten.com/g/200/200",
     twitter: "your_handle",
     notes: "Some notes",
     favorite: true,
-  };
-
+  }; */
+  const { contact } = useLoaderData();
   return (
     <div id="contact">
       <div>
-        <img
-          key={contact.avatar}
-          src={contact.avatar || null}
-        />
+        <img key={contact.avatar} src={contact.avatar || null} />
       </div>
 
       <div>
@@ -33,10 +30,7 @@ export default function Contact() {
 
         {contact.twitter && (
           <p>
-            <a
-              target="_blank"
-              href={`https://twitter.com/${contact.twitter}`}
-            >
+            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
               {contact.twitter}
             </a>
           </p>
@@ -52,11 +46,7 @@ export default function Contact() {
             method="post"
             action="destroy"
             onSubmit={(event) => {
-              if (
-                !confirm(
-                  "Please confirm you want to delete this record."
-                )
-              ) {
+              if (!confirm("Please confirm you want to delete this record.")) {
                 event.preventDefault();
               }
             }}
@@ -77,11 +67,7 @@ function Favorite({ contact }) {
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
-        aria-label={
-          favorite
-            ? "Remove from favorites"
-            : "Add to favorites"
-        }
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
       >
         {favorite ? "★" : "☆"}
       </button>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,36 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "../routes/Root";
+import {
+  createContactAction,
+  editContactAction,
+} from "../actions/contactsActions";
+import Contact from "../components/Contact";
+import EditContact from "../components/EditContact";
+import {
+  getContactLoader,
+  getContactsLoader,
+} from "../loader/getContactLoader";
 import ErrorPage from "../page/ErrorPage";
+import Root from "../routes/Root";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root/>,
-      errorElement: <ErrorPage />
-    },
-  ]);
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: getContactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: getContactLoader,
+        action: editContactAction,
+      },
+    ],
+  },
+]);
